f3bc6da2fe4148f3b159e5895924d76688b52a75
Pawel Kowalski <88.pawel.kowalski@gmail.com>
2020-04-19 23:50:12 +0200

    Added and configured IdentityServer project.

---
+++ ExpensesAPI.IdentityProvider/Config.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesAPI.IdentityProvider
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                new ApiResource("ExpensesAPI", "Expenses API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ExpensesSPAClient",
+                    //ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    RequirePkce = true,
+                    AllowAccessTokensViaBrowser = true,
+
+                    AllowedCorsOrigins = { "http://localhost:4200" },
+                    RedirectUris = { "http://localhost:4200/signin-callback" },
+                    PostLogoutRedirectUris = { "http://localhost:4200/signout-callback" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "ExpensesAPI"
+                    },
+
+                    AllowOfflineAccess = true
+                }
+            };
+    }
+}
