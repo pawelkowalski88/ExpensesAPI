5ef1c2e7056e5f540afec674d217e505bbf23cc6
Pawel Kowalski <88.pawel.kowalski@gmail.com>
2020-05-03 01:31:49 +0200

    Querying users moved to the new API

---
+++ ExpensesAPI.IdentityProvider.API/Controllers/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ExpensesAPI.IdentityProvider.API.Models;
+using ExpensesAPI.IdentityProvider.API.Resources;
+
+namespace ExpensesAPI.IdentityProvider.API.Controllers
+{ 
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResource>()
+                    .ForMember(ur => ur.Email,
+                        opt => opt.MapFrom(u => u.UserName));
+
+            CreateMap<UserResource, User>()
+                .ForMember(u => u.UserName,
+                    opt => opt.MapFrom(ur => ur.Email));
+        }
+    }
+}
---
+++ ExpensesAPI.IdentityProvider.API/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using ExpensesAPI.IdentityProvider.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExpensesAPI.IdentityProvider.API.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserAsync(string id);
+        //Task<IdentityUser> GetUserWithScopesAsync(string id);
+        Task<List<User>> GetUserListAsync(string query, string myId);
+        //Task SetSelectedScope(string userId, int scopeId);
+        //Task AddUser(string id, string name);
+    }
+}
